fc27a9330c280f8296d477860b4cab9d51d90a9b
madsn <m@madsn.net>
2013-02-24 22:55:51 +0100

    Android - confirm screen for shutdown and restart, saved credentials loaded on startup

---
+++ Android/Inventor/src/appinventor/ai_m/GTFU/Confirm.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"71","Source":"Form","Properties":{"$Name":"Confirm","$Type":"Form","$Version":"10","Uuid":"0","Title":"Confirm","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"2","Uuid":"-1886204236","Text":"Hey asshole, are you sure you want to do this? A shutdown or restart command will perform a hard shutdown of your fucking pc, this might cause data loss or corrupt your OS. Don't say I didn't fucking warn you.","TextAlignment":"1","Width":"-2"},{"$Name":"HorizontalArrangement1","$Type":"HorizontalArrangement","$Version":"2","Uuid":"627397245","Width":"-2","$Components":[{"$Name":"ConfirmBtn","$Type":"Button","$Version":"4","Uuid":"-1324485884","Text":"Fucking do it","Width":"-2"},{"$Name":"CancelBtn","$Type":"Button","$Version":"4","Uuid":"-974248674","Text":"Oh.. Never mind then","Width":"-2"}]}]}}
+|#
