1f7db34d8f2f92804a54b9470bf46f3905fe65c5
Mikkel Madsen <m@madsn.net>
2014-02-18 20:30:47 +0100

    fixed timestamp - only send command to clients if controller set cmdstate less than 2 minutes ago

--- flask/gtfu.py
+++ flask/gtfu.py
@@ -3,43 +3,43 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 
 class User:
-
+    
     def __init__(self):
         self.users = {
             "balls": {"user": "balls", "password": "jabadoo", "pwron": 0, "restart": 0, "pwroff": 0, "timestamp": datetime.now()},
             "odin": {"user": "odin", "password": "gtfu", "pwron": 0, "restart": 0, "pwroff": 0, "timestamp": datetime.now()},
             "thor": {"user": "thor", "password": "gtfu", "pwron": 0, "restart": 0, "pwroff": 0, "timestamp": datetime.now()}}
-
+        
     def login(self, name, password):
         print(self.users)
         if name in self.users:
             print("found user")
             if self.users[name]["password"] != password:
                 return None
-            if self.timestamp_expired(name):
-                self.reset_state(name)
             return self.users[name]
         else:
             print("user not found")
             return None
-
+        
     def timestamp_expired(self, name):
-        time_since_last_check = self.users[name]["timestamp"] - datetime.now()
-        command_expires_after = timedelta(minutes=5)
-        if time_since_last_check < command_expires_after:
-            return False
-        return True
-
+        timestampdiff = datetime.now() - self.users[name]["timestamp"]
+        if timestampdiff > timedelta(minutes=2):
+            return True
+        return False
+        
     def reset_state(self, name):
         user = self.users[name]
         user["pwron"] = 0
         user["pwroff"] = 0
         user["restart"] = 0
-        user["timestamp"] = datetime.now()
         self.users[name] = user
-
+    
+    def reset_timestamp(self, name):
+        self.users[name]["timestamp"] = datetime.now()
+        
     def update_cmdstate_and_get_response(self, userinfo, cmdstate):
         user = self.users[userinfo["user"]]
+        self.users[userinfo["user"]]["timestamp"] = datetime.now()
         if cmdstate == 1:
             user["pwron"] = 1
             return make_response("Fine, I'll wake the fucker up, now get lost", 202) #202-accepted
@@ -55,10 +55,13 @@ class User:
         else:
             return make_response("Unknown cmdstate");
 
-
-def get_poll_reply(userinfo):
+         
+def get_poll_reply(userinfo): 
     global user
     resp = ""
+    if user.timestamp_expired(userinfo["user"]):
+        user.reset_timestamp(userinfo["user"])
+        user.reset_state(userinfo["user"])
     if userinfo["pwron"]:
         # time to wake up
         # first reset the state
@@ -71,7 +74,7 @@ def get_poll_reply(userinfo):
         return make_response("0-Fuck off!", 200) #200-OK
     user.reset_state(userinfo["user"])
     return resp
-
+    
 
 @app.route("/api/poll/") # polling entry point, for arduino clients
 def embedded_entry_point():
@@ -83,7 +86,7 @@ def embedded_entry_point():
         return get_poll_reply(userinfo)
     else:
         return make_response("Who?? Go fuck yourself.", 404)
-
+    
 @app.route("/api/gtfu/") # entry point for android and other controller clients
 def controller_entry_point():
     global user
@@ -96,7 +99,8 @@ def controller_entry_point():
     else:
         return make_response("Who?? Go fuck yourself.", 404)
 
-
+    
 user = User()
 if __name__ == "__main__":
     app.run()
+
