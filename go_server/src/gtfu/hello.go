722e8e88affc418d4482ef6d12105779a87e6cdb
Mikkel Madsen <m@madsn.net>
2014-07-27 20:58:40 +0200

    martini

--- go_server/src/gtfu/hello.go
+++ go_server/src/gtfu/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import "github.com/go-martini/martini"
 
 func main() {
-    fmt.Printf("hello, world\n")
+  m := martini.Classic()
+  m.Get("/:name", func(params martini.Params) string {
+    return "Hello " + params["name"]
+  })
+
+  m.Get("/", func() string {
+    return "Hello world"
+  })
+
+  m.Run()
 }
